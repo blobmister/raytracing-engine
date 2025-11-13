5993cfe0eb23b8d9e5c78ec19515eb3ba1d37922
Krish Gupta <stockkrishgupta@gmail.com>
2025-11-13 17:54:19 +1100

    feat: Added a ray.h file to object-libraries sub-directory
    Header file contains a quick scaffold for a ray object type which stores
    a ray object in the parameterised line form of: r(t) = orig + t(dir).

--- src/main.cxx
+++ src/main.cxx
@@ -3,26 +3,13 @@
 #include <color.h>
 
 int main() {
+	double apsect_ratio = 16.0 / 9.0;
+	int image_width = 400;
 
-    // Image
+	int image_height = int(image_width / aspect_ratio);
+	image_height = (image_height < 1) ? 1 : image_height;
 
-    int image_width = 256;
-    int image_height = 256;
-
-    // Render
-
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-    for (int j = 0; j < image_height; j++) {
-	std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-	for (int i = 0; i < image_width; i++) {
-		auto pixel_color = color(
-					double(i)/(image_width+1),
-					double(j)/(image_width-1),
-		     			0
-				);
-		
-		write_color(std::cout, pixel_color);
-        }
-    }
+	double viewport_height = 2.0;
+	double viewport_width = viewport_height * (double(image_width/image_height));
 }
+
