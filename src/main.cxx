59f86b2d7d80ea71b9eace847819dbcce3320dbd
Krish Gupta <stockkrishgupta@gmail.com>
2025-12-03 13:36:24 +1100

    Refactored printing logic to camera.h

--- src/main.cxx
+++ src/main.cxx
@@ -2,99 +2,22 @@
 #include <hittable.h>
 #include <hittable-list.h>
 #include <sphere.h>
-
-
-color ray_color(const ray& r, const hittable& world) {
-	hit_record rec;
-	if (world.hit(r, interval(0, infinity), rec)) {
-		return 0.5 * (rec.normal + color(1,1,1));
-	}
-
-	vec3 unit_direction = unit_vector(r.direction());
-	auto a = 0.5*(unit_direction.y() + 1);
-	return (1.0-a)*color(1.0,1.0,1.0) + a*color(0.5,0.7,1.0);
-}
-
+#include <camera.h>
 
 
 int main() {
-	// Image
-	//
-	// A 16/9 aspect ratio is used. Actual pixel numbers are approximate
-	// A minimum height of 1 is set incase image_width is too low to maintain
-	// aspect ratio.
-
-	double aspect_ratio = 16.0 / 9.0;
-	int image_width = 400;
-
-	int image_height = int(image_width / aspect_ratio);
-	image_height = (image_height < 1) ? 1 : image_height;
-
-	// World
-	//
-	// Defines the world as a hittable list. This is where all objects are added and defined
 	
 	hittable_list world;
 
 	world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
 	world.add(make_shared<sphere>(point3(0,-100.5,-2), 100));
 
-	// Camera
-	//
-	// Camera centre is set to 0,0,0 for simplicity, and a focal length of 1 is also assumed
-	
-	auto focal_length = 1.0;
-	auto viewport_height = 2.0;
-	auto viewport_width = viewport_height * (double(image_width)/image_height);
-	auto camera_center = point3(0,0,0);
+	camera cam;
 
-	// Viewport vector def
-	//
-	// A separate coordinate system from the camera system is required to render the viewport image
-	// This is as (0, 0) on the viewport is assumed to be the top left of the viewport, but it is
-	// considered to be the centre on the camera coordinate system. 
-	//
-	// viewport_u represents the horiztonal vector from (0, 0), to the right edge of the veiwport and 
-	// viewport_v represents the vertical vertical from (0, 0), to the bottom edge of the veiwport.
-	
-	auto viewport_u = vec3(viewport_width, 0, 0);
-	auto viewport_v = vec3(0, -viewport_height, 0);
+	cam.aspect_ratio = 16.0 / 9.0;
+	cam.image_width = 400;
 
-	// Pixel-to-pixel gaps
-	//
-	// Pixels are taken to be 'dots' in the center of a grid. Since the grid requires
-	// boxes with width and height, pixels have gaps in between them. This represents that
-	// gap amount.
-
-	auto pixel_delta_u = viewport_u / image_width;
-	auto pixel_delta_v = viewport_v / image_height;
-
-	// Location of upper left pixel
-	
-	auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
-	auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-
-	// Render
-	//
-	// Below is rendering logic for desired image in ppm format.
-	
-
-	std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
-
-	for (int j = 0; j < image_height; j++) {
-		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-		for (int i = 0; i < image_width; i++) {
-			auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-			auto ray_direction = pixel_center - camera_center;
-			ray r(camera_center, ray_direction);
-
-			color pixel_color = ray_color(r, world);
-			write_color(std::cout, pixel_color);
-		}
-	}
-
-	std::clog << "\rDone.					\n";
+	cam.render(world);
 
 }
 
